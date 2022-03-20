1440bbe9739988658ecc2d9e2900097181578c3e
bournejj <cras.jbourne@gmail.com>
2022-03-20 16:40:10 +0000

    carousel testing

---
+++ carousel/src/Carousel.test.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import Carousel from "./Carousel";
+import Card from "./Card";
+
+
+it("renders without crashing", function() {
+  render(<Carousel />);
+});
+
+it("matches snapshot", function() {
+  const {asFragment} = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+
+it("works when you click on the right arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+
+  // move back in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the third image to show, but not the second
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Josh Post on Unsplash")).toBeInTheDocument();
+});
+it("left arrow hides when on first image", function() {
+  const { queryByTestId } = render(<Carousel />);
+
+
+  // move back in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+
+
+  // expect the third image to show, but not the second
+  expect(leftArrow).not.toBeInTheDocument();
+ 
+});
+it("right arrow hides when on last image", function() {
+  const { queryByTestId } = render(<Carousel />);
+
+
+  // move back in the carousel
+  const rightArrow = queryByTestId("left-arrow");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+
+
+  // expect the third image to show, but not the second
+  expect(rightArrow).not.toBeInTheDocument();
+ 
+});
